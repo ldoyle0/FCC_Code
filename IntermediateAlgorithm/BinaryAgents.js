31533d477bf379d0ce6da8ba1b4a095933bbdd23
Lee T. Doyle <ldoyle0@gmail.com>
2016-08-17 16:16:34 +0000

    completed binary agents

---
+++ IntermediateAlgorithm/BinaryAgents.js
@@ -0,0 +1,15 @@
+
+function binaryAgent(str) {
+  
+  var strArr = str.split(" ");
+  
+  var newString = "";
+  
+  for (var i = 0; i < strArr.length; i++){
+    newString += String.fromCharCode(parseInt(strArr[i],2));
+  }
+  
+  return newString;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
