c59033d5ee74cdd0bce92e71f26ab5a12c4eb90e
Lee T. Doyle <ldoyle0@gmail.com>
2016-08-17 15:38:50 +0000

    completed wherefore art thou

--- IntermediateAlgorithm/WhereforeArtThou.js
+++ IntermediateAlgorithm/WhereforeArtThou.js
@@ -1,16 +1,17 @@
-
 function whatIsInAName(collection, source) {
-  // What's in a name?
-  var arr = [];
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
   var keys = Object.keys(source);
-  // Only change code below this line
-  for (var i = 0; i < collection.length; i++){
-    if (collection[i].hasOwnProperty(keys) && collection[i][keys] == source[keys]){
-      arr.push(collection[i]);
-    }
-  }
-  // Only change code above this line
-  return keys;
-}
 
+  // filter the collection
+  return collection.filter(function (obj) {
+    for(var i = 0; i < keys.length; i++) {
+      if(!obj.hasOwnProperty(keys[i]) || obj[keys[i]] !== source[keys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
 whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
