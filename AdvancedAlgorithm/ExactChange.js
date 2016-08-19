a3f355730d05bf52f35ae0fc57fc8d75fc54fadf
Lee T. Doyle <ldoyle0@gmail.com>
2016-08-19 19:56:40 +0000

    updated exact change

--- AdvancedAlgorithm/ExactChange.js
+++ AdvancedAlgorithm/ExactChange.js
@@ -1,8 +1,17 @@
 
 function checkCashRegister(price, cash, cid) {
-  var change;
+  var change = cash - price;
+  var totalcid = 0;
   // Here is your change, ma'am.
-  return change;
+  for (var i = 0; i < cid.length; i++){
+    totalcid += cid[i][1];
+  }
+  
+  if (totalcid > change){
+    
+  }
+  
+  return totalcid;
 }
 
 // Example cash-in-drawer array:
@@ -16,4 +25,5 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], 
+["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
