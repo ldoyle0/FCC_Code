edf54f436333cb26ecfa66a74db263f00d713dd2
Lee T. Doyle <ldoyle0@gmail.com>
2016-08-19 18:49:53 +0000

    started symmetric and exact change advanced projects

--- AdvancedAlgorithm/SymmetricDifference.js
+++ AdvancedAlgorithm/SymmetricDifference.js
@@ -1,16 +1,20 @@
-function sym(args) {
+function sym() {
   
-  var master = []
+  var master = [];
   
-  for (var i = 0; i < args.length; i++){
-    master.push(args[i]);
+  for (var i = 0; i < this.length; i++){
+    var current = this[i];
+    if(master.indexOf(current) < 0){
+      master.push(current);
+    }
   }
   
-  var flatArr = master.reduce(function(a,b){
-    return a.concat(b);
-  },[]);
-  
-  return args.length;
+//  var flatArr = master.reduce(function(a,b){
+//    return a.concat(b);
+//  },[]);
+//  
+//  return flatArr;
+  return master;
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
